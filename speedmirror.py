6ccb3a6ed31d8fc5671aed24de2929bf6096a299
Yoan Ancelly <git@yoanancelly.fr>
2022-10-16 12:58:31 +0200

    Ping works

--- speedmirror.py
+++ speedmirror.py
@@ -5,39 +5,62 @@ import requests
 from pythonping import ping
 import bs4 as BeautifulSoup
 
-response = requests.get("https://http.kali.org/README.mirrorlist")
-soup = BeautifulSoup.BeautifulSoup(response.text, "html.parser")
-
 def contains_readme(url):
     if url.endswith("/README"):
         return True
     else:
         return False
 
+# Check if user is root
 def check_root():
     if os.geteuid() != 0:
-        return False
-    else:
-        return True
+        print("You must be root to run this script.")
+        exit(1)
 
 # Ping url and return avg ping
 def ping_url(url):
     print(f'Pinging: {url}')
-    return ping(url, verbose=True).rtt_avg_ms
+    return ping(url).rtt_avg_ms
+
+def ping_all_urls(domains):
+    return list(map(ping_url, domains))
 
 def modify_source_list():
     pass
 
-if __name__ == "__main__":
+def change_to_http():
+    pass
 
-    # Check if the user is root
-    if not check_root():
-        print("You must be root to run this script.")
-        exit(1)
-
+# Get all links from a url
+def find_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup.BeautifulSoup(response.text, "html.parser")
     links = list(map(lambda x: x.get("href"), soup.find_all("a"))) # Get all links
     readme_links = list(filter(contains_readme, links)) # Filter out all links that don't contain "README"
     final_links = list(map(lambda x: x.replace('/README', ''), readme_links)) # remove /README from the end of the link
-    final_links = list(set(final_links)) # Remove duplicates
-    domains = list(map(lambda x: x.split("/")[2], final_links)) # Get the domain name from the link
-    print(ping_url(domains[0]))
+    return list(set(final_links)) # Remove duplicates
+
+# Get domains from links
+def get_domains(links):
+    return list(map(lambda x: x.split('/')[2], links))
+
+def get_fastest_mirror(domains):
+    pings = ping_all_urls(domains)
+    return pings.index(min(pings)), domains[pings.index(min(pings))], min(pings)
+
+if __name__ == "__main__":
+
+    url = "https://http.kali.org/README.mirrorlist"
+
+    # Check if the user is root
+    check_root()
+
+    links = find_links(url)
+    domains = get_domains(links)
+    best_mirror = get_fastest_mirror(domains)
+    print('--------------------------------')
+    print(best_mirror)
+    print(f'index: {best_mirror[0]}')
+    print(f'domain: {best_mirror[1]}')
+    print(f'link: {links[best_mirror[0]]}')
+    print(f'avg speed: {best_mirror[2]}ms')
